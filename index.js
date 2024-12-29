c035ebb0795791c40d088e240824428b4e9b2417
Rishabh Gupta <rishabh@Rishabhs-MacBook-Air.local>
2024-12-29 18:04:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+console.log("Hello World");
+
+
+const express = require('express') // made an object that contains express
+const app = express() // now we csn use app.abc
+const port = 2000 // set the port our server will be listeing onto
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) =>{
+    res.send('Rishabh.com')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1> Login Successful</h1>')
+})
+
+app.get('/youtube', (req,res) =>{
+    res.send('<a href = https://www.youtube.com/watch?v=7fjOw8ApZ1I> link <\a>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`) // search http://localhost:3000
+})
